374dd8a38e30727b8653e9a76aa878398244f8b0
AswathiMohan23 <achu999here@gmail.com>
2022-10-21 17:41:36 +0530

    [REFACTORED & ADDED] : refactored the code and added lines to find the lucky days

--- src/com/java/Gambling.java
+++ src/com/java/Gambling.java
@@ -1,9 +1,12 @@
 package com.java;
 
 public class Gambling {
-    static int won = 0, lost = 0,wonCount=0,lostCount=0;
+    static int won = 0, lost = 0, wonCount = 0, lostCount = 0;
+    static int[] luckyDay =new int[35];
+    static int[] badDay =new int[35];
+
     public static void main(String[] args) {
-        int i;
+        int i=0,j=0;
         GamblingDetails gambling = new GamblingDetails();
         System.out.println("Gambling stake and bet per game : " + gambling.toString());
         while ((won < 50) && (lost < 50)) {
@@ -13,28 +16,35 @@ public class Gambling {
             System.out.println("won 50$ so you can resign from the game");
         else if ((lost == 50))
             System.out.println("lost 50$ you can resign from the game");
-        for (i = 1; i <= 20; i++) {
+        for (i = 1; i <= 30; i++) {
             String result = checkResultCount();
             System.out.println("Day " + i + " = " + result);
+            if(result=="Won LUCKY DAY!!!")
+                luckyDay[i]=i;
+            else if(result!="Won LUCKY DAY!!!")
+                badDay[i]=i;
         }
-        printing(20);
-        //Each month would like to know the days won and lost and how much?
-        //gave i<=10 because in previous line already checked 20 times and question is to check for 30 days
-        // since already I have given count as "public static wonCount=0,lostCount=0",
-        // already count after 20 days will be added so I am taking 10 here so that 10+20=30
-        // and wonCount or lost count will have exact value
-
-        for(i=1;i<=10;i++){
-            String result = checkResultCount();
-            System.out.println("Day " + i + " = " + result);
-        }
+        System.out.println("lucky days are : " );
+        printingArray(luckyDay);
+        System.out.println("Bad days are : " );
+        printingArray(badDay);
         printing(30);// need to find count for 30 days
+    }
 
 
-}
+
     private static void printing(int i) {
         System.out.println("total no of times the gambler won the game in "+i+" days = " + wonCount);
         System.out.println("total no of times the gambler lost the game in "+i+" days = " + lostCount);
+    }
+    private static void printingArray(int[] array) {
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                array[j] = i;
+                System.out.println(array[j]);
+            }
+        }
     }
 
     private static String checkResultCount() {
@@ -42,10 +52,10 @@ public class Gambling {
         switch (randomCheck) {
             case 1:
                 wonCount++;
-                return "won";
+                return "Won LUCKY DAY!!!";
             case 0:
                 lostCount++;
-                return "lost";
+                return "Lost BAD DAY!!!";
             default:
                 break;
         }
--- src/com/java/GamblingDetails.java
+++ src/com/java/GamblingDetails.java
@@ -1,15 +1,21 @@
 package com.java;
 
+import java.util.Arrays;
+
 public class GamblingDetails {
    private final int stake=100; // in dollars
    private final int bet=1;    //in dollars
 
+
     public int getStake() {
         return stake;
     }
+
     public int getBet() {
         return bet;
     }
+
+
 
     @Override
     public String toString() {
