a2eb4c21411200f1f8c03cf73d9ec5c18a9d8492
AswathiMohan23 <achu999here@gmail.com>
2022-10-21 11:02:11 +0530

    [ADDED] : code to count the no of times gambler has won or los the game in 30 days

--- src/com/java/Gambling.java
+++ src/com/java/Gambling.java
@@ -18,7 +18,20 @@ public class Gambling {
             System.out.println("Day " + i + " = " + result);
         }
         printing(20);
-    }
+        //Each month would like to know the days won and lost and how much?
+        //gave i<=10 because in previous line already checked 20 times and question is to check for 30 days
+        // since already I have given count as "public static wonCount=0,lostCount=0",
+        // already count after 20 days will be added so I am taking 10 here so that 10+20=30
+        // and wonCount or lost count will have exact value
+
+        for(i=1;i<=10;i++){
+            String result = checkResultCount();
+            System.out.println("Day " + i + " = " + result);
+        }
+        printing(30);// need to find count for 30 days
+
+
+}
     private static void printing(int i) {
         System.out.println("total no of times the gambler won the game in "+i+" days = " + wonCount);
         System.out.println("total no of times the gambler lost the game in "+i+" days = " + lostCount);
