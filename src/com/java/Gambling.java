e3154926488319790e8fe592c26849bf37961df3
AswathiMohan23 <achu999here@gmail.com>
2022-10-21 10:27:08 +0530

    [ADDED] : code to display bet value and stake value

---
+++ src/com/java/Gambling.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class Gambling {
+
+    public static void main(String[] args) {
+        GamblingDetails gambling = new GamblingDetails();
+        System.out.println("Gambling stake and bet per game : " + gambling.toString());
+    }
+}
+
